a8ab2334db252dd15a538087408896e2a211b987
Sheila Kagawa <145503159+shlqjn@users.noreply.github.com>
2025-07-09 23:03:36 -0700

    Removed Forecast HTML, CSS,JS

--- src/index.js
+++ src/index.js
@@ -54,49 +54,8 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-  return days[date.getDay()];
-}
 //Search for the city
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Seattle");
-
-function getForecast(city) {
-  let apiKey = "5b7e32442badt617fc2dae0afo330c92";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
-  axios(apiUrl).then(displayForecast);
-}
-
-function displayForecast(response) {
-  let forecast = document.querySelector("#forecast");
-
-  let forecastHtml = "";
-
-  response.data.daily.forEach(function (day, index) {
-    if (index < 5) {
-      forecastHtml =
-        forecastHtml +
-        `<div class="weather-forecast-day">
-      <div class="weather-forecast-date">${formatDay(day.time)}</div>
-      
-      <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
-     
-      <div class="weather-forecast-temperatures">
-        <div class="weather-forecast-temperature">
-          <strong>${Math.round(day.temperature.maximum)}º</strong>
-        </div>
-        <div class="weather=forecast-temperature">${Math.round(
-          day.temperature.minimum
-        )}°</div>
-        </div>
-        </div>`;
-    }
-  });
-  let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = forecastHtml;
-}
